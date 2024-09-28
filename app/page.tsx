fdf0cf7417f331ae4250eeb4c876e0f572a23802
Beatriz Santiago <beatrizsantiago1999@hotmail.com>
2024-09-28 11:45:16 -0300

    ✨ Add home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,90 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import Landing from "./components/Landing";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+    <Landing>
+      <div className="flex flex-col items-center justify-center md:flex-row">
+        <h1 className="text-lg font-semibold m-3 max-w-[300px] leading-7 lg:text-xl lg:max-w-[430px]">
+          Experimente mais liberdade no
+          controle da sua vida financeira.
+          Crie sua conta com a gente!
+        </h1>
+        <div className="w-[660px] h-[410px] relative m-4">
+          <Image
+            src="/images/banner.svg"
+            alt="Banner"
+            objectFit="contain"
+            fill
+          />
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+      </div>
+
+      <h2 className="text-lg text-center my-5">
+        <b>Vantagens do nosso banco:</b>
+      </h2>
+
+      <div className="flex justify-center gap-4 flex-wrap">
+        <div className="flex flex-col items-center max-w-[282px]">
           <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+            src="/images/gift.svg"
+            alt="Vantagem 1"
+            width={73}
+            height={56}
           />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+          <p className="text-lg text-center text-secondary-main font-bold my-2">
+            Conta e cartão gratuitos
+          </p>
+          <p className="text-center text-gray-600 leading-5">
+            Isso mesmo, nossa conta é digital, sem custo fixo e mais que isso: sem tarifa de manutenção.
+          </p>
+        </div>
+
+        <div className="flex flex-col items-center max-w-[282px]">
           <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
+            src="/images/withdraw.svg"
+            alt="Vantagem 1"
+            width={73}
+            height={56}
           />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+          <p className="text-lg text-center text-secondary-main font-bold my-2">
+            Saques sem custo
+          </p>
+          <p className="text-center text-gray-600 leading-5">
+            Você pode sacar gratuitamente 4x por mês de qualquer Banco 24h.
+          </p>
+        </div>
+
+        <div className="flex flex-col items-center max-w-[282px]">
           <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
+            src="/images/star.svg"
+            alt="Vantagem 1"
+            width={73}
+            height={56}
           />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+          <p className="text-lg text-center text-secondary-main font-bold my-2">
+            Programa de pontos
+          </p>
+          <p className="text-center text-gray-600 leading-5">
+            Você pode acumular pontos com suas compras no crédito sem pagar mensalidade!
+          </p>
+        </div>
+
+        <div className="flex flex-col items-center max-w-[282px]">
+          <Image
+            src="/images/star.svg"
+            alt="Vantagem 1"
+            width={73}
+            height={56}
+          />
+          <p className="text-lg text-center text-secondary-main font-bold my-2">
+            Seguro Dispositivos
+          </p>
+          <p className="text-center text-gray-600 leading-5">
+            Seus dispositivos móveis (computador e laptop) protegidos por uma mensalidade simbólica.
+          </p>
+        </div>
+      </div>
+    </Landing>
   );
 }
