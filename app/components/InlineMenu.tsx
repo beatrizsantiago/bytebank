953da07a9080ba8965ccd97f3e830b940c3a4572
Beatriz Santiago <beatrizsantiago1999@hotmail.com>
2024-09-30 19:52:22 -0300

    ✨ Add InlineMenu component

---
+++ app/components/InlineMenu.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { Menu } from '@beatrizsantiago/money-flow';
+import { useRouter } from 'next/navigation';
+
+const InlineMenu = () => {
+  const router = useRouter();
+
+  return (
+    <div className="hidden md:flex justify-center lg:hidden w-full">
+      <Menu
+        activeIndex={0}
+        items={[
+          { title: 'Início', onClick: () => router.push('/dashboard') },
+          { title: 'Transferências', onClick: () => router.push('/transferencias') },
+          { title: 'Investimentos', onClick: () => router.push('/investimentos') },
+          { title: 'Outros serviços', onClick: () => router.push('/outros') },
+        ]}
+        inline
+      />
+    </div>
+  );
+};
+
+export default InlineMenu;
