02432174ebc5c05f43bd532a31ea609149113919
Beatriz Santiago <beatrizsantiago1999@hotmail.com>
2024-10-01 18:07:13 -0300

    ✨ Get dashboard data to Jumbotron

--- app/components/Jumbotron.tsx
+++ app/components/Jumbotron.tsx
@@ -1,12 +1,20 @@
 'use client';
 
 import { useState } from 'react';
-import { EyeFilled, EyeInvisibleFilled } from '@ant-design/icons';
+import { EyeFilled, EyeInvisibleFilled, LoadingOutlined } from '@ant-design/icons';
 import { formatDate } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
 import Image from 'next/image';
+import useSWR from 'swr';
+
+import { DashboardService } from '../services/dashboard';
+import { money } from '../utils/formats';
 
 const Jumbotron = () => {
+  const dashboardService = new DashboardService();
+
+  const { data } = useSWR('dashboard', () => dashboardService.getData());
+
   const [showBalance, setShowBalance] = useState(false)
 
   const today = new Date();
@@ -37,36 +45,45 @@ const Jumbotron = () => {
         className="absolute left-8 bottom-8 z-0"
       />
 
-      <div className="flex flex-col items-center mb-6 sm:mr-6 sm:mb-0 sm:items-start z-10">
-        <h1 className="font-semibold text-xl leading-6">
-          Olá, teste :)
-        </h1>
-        <p className="text-sm mt-6">
-          {formatDate(today, 'EEEE, dd/MM/yyyy', { locale: ptBR })}
-        </p>
-      </div>
+      {!data ? (
+        <div className="w-full flex flex-col items-center justify-center">
+          <LoadingOutlined className="text-xl" />
+          <p className="text-sm">Carregando...</p>
+        </div>
+      ) : (
+        <>
+          <div className="flex flex-col items-center mb-6 sm:mr-6 sm:mb-0 sm:items-start z-10">
+            <h1 className="font-semibold text-xl leading-6">
+              Olá, teste :)
+            </h1>
+            <p className="text-sm mt-6">
+              {formatDate(today, 'EEEE, dd/MM/yyyy', { locale: ptBR })}
+            </p>
+          </div>
 
-      <div className="flex justify-center sm:flex-1 sm:items-center z-10">
-        <div>
-          <div className="flex items-center">
-            <p className="text-lg font-semibold mr-5">Saldo</p>
-            <div className="cursor-pointer" onClick={() => setShowBalance((current) => !current)}>
-              {showBalance ? (
-                <EyeInvisibleFilled className="text-lg" />
-              ) : (
-                <EyeFilled className="text-lg" />
-              )}
+          <div className="flex justify-center sm:flex-1 sm:items-center z-10">
+            <div>
+              <div className="flex items-center">
+                <p className="text-lg font-semibold mr-5">Saldo</p>
+                <div className="cursor-pointer" onClick={() => setShowBalance((current) => !current)}>
+                  {showBalance ? (
+                    <EyeInvisibleFilled className="text-lg" />
+                  ) : (
+                    <EyeFilled className="text-lg" />
+                  )}
+                </div>
+              </div>
+              <div className="bg-white w-[180px] h-[2px] my-4" />
+              <p className="mb-2">
+                Conta Corrente
+              </p>
+              <p className="text-3xl">
+                {showBalance ? money(data.totalValue) : 'R$ ********'}
+              </p>
             </div>
           </div>
-          <div className="bg-white w-[180px] h-[2px] my-4" />
-          <p className="mb-2">
-            Conta Corrente
-          </p>
-          <p className="text-3xl">
-            {showBalance ? 'R$ 2.000,00' : 'R$ ********'}
-          </p>
-        </div>
-      </div>
+        </>
+      )}
     </div>
   );
 };
