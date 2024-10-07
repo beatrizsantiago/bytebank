4e18050fcc5ecc15c0d631642c2d2bc08cbdaa51
Beatriz Santiago <beatrizsantiago1999@hotmail.com>
2024-10-07 20:16:15 -0300

    ✨ List, edit and delete a transaction

---
+++ app/components/StatementList/components/EditModal.tsx
@@ -0,0 +1,115 @@
+'use client';
+
+import { useState } from 'react';
+import { toast } from 'react-toastify';
+import { Input, Select, Button } from '@beatrizsantiago/money-flow';
+
+import { KindType, TransactionData, TransactionService } from '../../../services/transactions'
+import Modal from '../../Modal';
+
+type Props = {
+  onClose: () => void;
+  transactionId: string;
+};
+
+type OptionType = {
+  label: string;
+  value: string;
+};
+
+const EditTransactionModal = ({ onClose, transactionId }:Props) => {
+  const [kind, setKind] = useState<OptionType | null>(null)
+  const [value, setValue] = useState('');
+  const [errors, setErrors] = useState<{ [key:string]: string } | null>(null)
+
+  const onEditClick = async () => {
+    const floatValue = value ? parseFloat(value.replace(',', '.')) : 0;
+
+    if (!kind) {
+      setErrors({ kind: 'Selecione o tipo de transação' });
+      return;
+    };
+
+    if (floatValue <= 0) {
+      setErrors({ value: 'O valor da transação deve ser maior que zero' });
+      return;
+    };
+
+    setErrors(null);
+
+    const transactionService = new TransactionService();
+
+    try {
+      const amount = kind.value === 'TRANSFER' ? (floatValue * -1) : floatValue;
+
+      await transactionService.update(transactionId, new TransactionData(kind.value as KindType, amount));
+
+      toast.success('Transação atualizada com sucesso!', {
+        onClose: () => window.location.reload(),
+      });
+
+      setKind(null);
+      setValue('');
+    } catch (error) {
+      if (error instanceof Error) {
+        toast.error(error.message);
+      } else {
+        toast.error('Erro ao realizar transação');
+      }
+    };
+  };
+
+  return (
+    <Modal isVisible onClose={onClose}>
+      <div className="w-full flex flex-col items-center z-10 relative">
+        <h2 className="font-bold text-center text-lg text-primary-main mb-8">
+          Editar transação
+        </h2>
+
+        <div className="w-full">
+          <div className="mb-6">
+            <Select
+              placeholder="Selecione o tipo de transação"
+              options={[
+                {
+                  label: 'Transferência',
+                  value: 'TRANSFER',
+                },
+                {
+                  label: 'Depósito',
+                  value: 'DEPOSIT',
+                },
+              ]}
+              selected={kind}
+              onChange={(opt) => setKind(opt)}
+            />
+            {errors?.kind && <p className="text-red-500 text-sm">{errors.kind}</p>}
+          </div>
+
+          <p className="font-semibold text-primary-main mb-1 text-sm">
+            Valor
+          </p>
+          <div className="mb-6">
+            <Input
+              placeholder="0,00"
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
+              error={!!errors?.value}
+              className="w-full"
+              type="number"
+            />
+            {errors?.value && <p className="text-red-500 text-sm">{errors.value}</p>}
+          </div>
+        </div>
+
+        <Button
+          text="Atualizar"
+          className="max-w-[250px]"
+          onClick={onEditClick}
+        />
+      </div>
+    </Modal>
+  );
+};
+
+export default EditTransactionModal;
---
+++ app/components/StatementList/components/Item.tsx
@@ -0,0 +1,54 @@
+'use client';
+
+import { useState } from 'react';
+import { StatementItem } from '@beatrizsantiago/money-flow';
+import { toast } from 'react-toastify';
+
+import { TransactionService, ITransactionData } from '../../../services/transactions'
+import EditModal from './EditModal';
+
+type Props = {
+  transaction: ITransactionData;
+}
+
+const Item = ({ transaction }:Props) => {
+  const transactionService = new TransactionService();
+
+  const [showEditModal, setShowEditModal] = useState(false);
+
+  const onDeleteClick = async () => {
+    try {
+      await transactionService.remove(transaction._id);
+      toast.success('Transação removida com sucesso!', {
+        onClose: () => window.location.reload(),
+      });
+    } catch (error) {
+      if (error instanceof Error) {
+        toast.error(error.message);
+      } else {
+        toast.error('Erro ao deletar a transação');
+      }
+    };
+  };
+
+  return (
+    <>
+      <StatementItem
+        date={transaction.date}
+        value={transaction.value}
+        kind={transaction.kind}
+        onDeleteClick={onDeleteClick}
+        onEditClick={() => setShowEditModal(true)}
+      />
+
+      {showEditModal && (
+        <EditModal
+          onClose={() => setShowEditModal(false)}
+          transactionId={transaction._id}
+        />
+      )}
+    </>
+  );
+};
+
+export default Item;
