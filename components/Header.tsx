934b43135b4611caf8c991d99d2921047d2c5c9e
Beatriz Santiago <beatrizsantiago1999@hotmail.com>
2024-10-24 20:55:31 -0300

    ✨ Add logout and user name on Header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,36 +1,75 @@
 'use client';
 
 import { useState } from 'react';
+import { useRouter } from 'next/navigation';
 import { CloseOutlined, MenuOutlined, UserOutlined } from '@ant-design/icons';
+import { Menu } from 'money-flow';
 
-import Menu from './Menu';
+import NavigationMenu from './NavigationMenu';
+import LocalStorage from '../services/localStorage';
 
 const Header = () => {
-  const [openMenu, setOpenMenu] = useState(false)
+  const router = useRouter();
 
-  const toggleMenu = () => setOpenMenu(!openMenu);
+  const localStorageService = new LocalStorage();
+
+  const userInfo = localStorageService.getUserInfoFromToken();
+
+  const [openNavigationMenu, setOpenNavigationMenu] = useState(false);
+  const [openUserMenu, setOpenUserMenu] = useState(false);
+
+  const toggleNavigationMenu = () => {
+    setOpenUserMenu(false);
+    setOpenNavigationMenu((current) => !current);
+  };
+
+  const toggleUserMenu = () => {
+    setOpenNavigationMenu(false);
+    setOpenUserMenu((current) => !current);
+  };
+
+  const logout = () => {
+    localStorageService.clearToken();
+    router.replace('/')
+  };
 
   return (
     <div className="w-full h-24">
       <nav className="fixed top-0 w-full h-24 p-5 bg-primary-main flex justify-center items-center z-50">
         <div className="w-full max-w-[1200px] flex items-center justify-between md:justify-end">
           <div className="block md:hidden">
-            <MenuOutlined className="text-secondary-main font-semibold text-xl" onClick={toggleMenu} />
+            <MenuOutlined className="text-secondary-main font-semibold text-xl" onClick={toggleNavigationMenu} />
 
-            {openMenu && (
+            {openNavigationMenu && (
               <div className="absolute bg-secondary-light p-3 left-0 top-0">
                 <div className="flex justify-end">
-                  <CloseOutlined className="text-secondary-main text-lg" onClick={toggleMenu} />
+                  <CloseOutlined className="text-secondary-main text-lg" onClick={toggleNavigationMenu} />
                 </div>
-                <Menu />
+                <NavigationMenu />
               </div>
             )}
           </div>
 
           <div className="flex items-center">
-            <p className="text-white font-semibold text-sm mr-4 md:mr-8">Harry Potter</p>
+            <p className="text-white font-semibold text-sm mr-4 md:mr-8">{userInfo.user_name}</p>
             <div className="w-10 h-10 border-2 border-secondary-main rounded-3xl flex items-center justify-center">
-              <UserOutlined className="text-secondary-main text-2xl font-light" />
+              <UserOutlined className="text-secondary-main text-2xl font-light cursor-pointer" onClick={toggleUserMenu} />
+
+              {openUserMenu && (
+                <div className="absolute bg-black p-3 right-0 top-0">
+                  <div className="flex justify-end">
+                    <CloseOutlined className="text-secondary-main text-lg" onClick={toggleUserMenu} />
+                  </div>
+                  <Menu
+                    activeIndex={0}
+                    items={[
+                      { title: 'Minha conta', onClick: () => router.push('/perfil') },
+                      { title: 'Sair', onClick: () => logout() },
+                    ]}
+                    isWhite
+                  />
+                </div>
+              )}
             </div>
           </div>
         </div>
